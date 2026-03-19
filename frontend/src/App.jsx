c9b0790d94844b5b77244479ee0b85b0100c9ce3
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2026-03-20 02:16:29 +0530

    Home page atteched in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
