8a6220a1a99e13729541cb538b940589e78d07a4
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2026-03-20 19:05:16 +0530

    add adduser Path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./assets/pages/Home";
+import AddUser from "./assets/pages/AddUser";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddUser />} />
       </Routes>
     </BrowserRouter>
   );
